6ffcd7aa1434e3558114ac01bac3099ff7fb67f0
canfoo <canfoo@126.com>
2018-12-27 19:40:48 +0800

    u

--- build/main.bundle.js
+++ build/main.bundle.js
@@ -2,19 +2,13 @@ webpackJsonp([0],[
 /* 0 */,
 /* 1 */
 /***/ (function(module, exports, __webpack_require__) {
-
 "use strict";
-
-
 // main.js
-
 var world = __webpack_require__(0);
-
 function app() {
   console.log('hello word');
 }
 app();
 world();
-
 /***/ })
 ],[1]);
